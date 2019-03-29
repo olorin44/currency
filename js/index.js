28dcdaa4b107bda20b5f3e731395d6e63119b661
Piotr Bernatowicz <piotrbernatowicz@tutanota.com>
2019-03-30 00:41:43 +0100

    Change api, return fetch to class component, add select currency option

--- js/index.js
+++ js/index.js
@@ -1,13 +1,17 @@
 // const urlCurrency = "https://api.nbp.pl/api/exchangerates/rates/a/";
+
 // let currencyDataTableAll = [];
+
 // const getCurrencyData = () => {
 //   fetch(urlCurrency + "GBP")
 //     .then(resp => resp.json())
 //     .then(resp => {
 //       let currencyDataGBP = {
+//         id: 1,
 //         code: resp.code,
 //         name: resp.currency,
-//         rate: resp.rates[0].mid
+//         rate: resp.rates[0].mid,
+//         date: resp.rates[0].effectiveDate
 //       };
 //       currencyDataTableAll.push(currencyDataGBP);
 //     });
@@ -15,9 +19,11 @@
 //     .then(resp => resp.json())
 //     .then(resp => {
 //       let currencyDataUSD = {
+//         id: 2,
 //         code: resp.code,
 //         name: resp.currency,
-//         rate: resp.rates[0].mid
+//         rate: resp.rates[0].mid,
+//         date: resp.rates[0].effectiveDate
 //       };
 //       currencyDataTableAll.push(currencyDataUSD);
 //     });
@@ -25,9 +31,11 @@
 //     .then(resp => resp.json())
 //     .then(resp => {
 //       let currencyDataEUR = {
+//         id: 3,
 //         code: resp.code,
 //         name: resp.currency,
-//         rate: resp.rates[0].mid
+//         rate: resp.rates[0].mid,
+//         date: resp.rates[0].effectiveDate
 //       };
 //       currencyDataTableAll.push(currencyDataEUR);
 //     });
@@ -35,6 +43,7 @@
 //     .then(resp => resp.json())
 //     .then(resp => {
 //       let currencyDataCHF = {
+//         id: 4,
 //         code: resp.code,
 //         name: resp.currency,
 //         rate: resp.rates[0].mid,
@@ -46,154 +55,21 @@
 
 // window.onload = getCurrencyData();
 
-// const dupa = () => {
-//   console.log(currencyDataTableAll);
-// };
-
-// const Cash = props => {
-//   const value = (props.cash / props.ratio).toFixed(2);
-//   return (
-//     <div>
-//       {props.title} {props.cash <= 0 ? "" : value}
-//     </div>
-//   );
-// };
-
-// class CurrencyCheck extends React.Component {
-//   state = {
-//     amount: ""
-//   };
-
-//   currencies = [
-//     {
-//       id: 1,
-//       name: "numerek 1: ",
-//       ratio: 2.5,
-//       title: "chuj"
-//     },
-//     {
-//       id: 2,
-//       name: "numerek 2",
-//       ratio: 4.5,
-//       title: "wartość nr 2: "
-//     },
-//     {
-//       id: 3,
-//       name: "numerek 3",
-//       ratio: 1.7,
-//       title: "wartość nr 3: "
-//     },
-//     {
-//       id: 4,
-//       name: "numerek 4",
-//       ratio: 3.8,
-//       title: "wartość nr 4: "
-//     }
-//   ];
-
-//   handleChange = event => {
-//     this.setState({
-//       amount: event.target.value
-//     });
-//   };
-
-//   render() {
-//     const { amount } = this.state;
-
-//     const calculators = this.currencies.map(currency => (
-//       <Cash
-//         key={currency.id}
-//         ratio={currency.ratio}
-//         title={currency.title}
-//         cash={amount}
-//       />
-//     ));
-//     return (
-//       <div className="app">
-//         <button onClick={dupa}>dupa</button>
-//         <label>
-//           <input
-//             type="number"
-//             value={this.state.amount}
-//             onChange={this.handleChange}
-//           />
-//         </label>
-//         {calculators}
-//       </div>
-//     );
-//   }
-// }
-
-// ReactDOM.render(<CurrencyCheck />, document.getElementById("root"));
-
-//////////////////////////////////////////////////////////////////////////////////////
-
-// const urlCurrency = "https://api.nbp.pl/api/exchangerates/rates/a/";
-
-// // window.onload = getCurrencyData();
-
 // const Cash = props => {
 //   const value = (props.cash / props.rate).toFixed(2);
 //   return (
 //     <div>
-//       {props.code} {props.cash <= 0 ? "" : value}
+//       {props.cash} {"PLN You can exchange for:"} {props.cash <= 0 ? "" : value}{" "}
+//       {props.code} {", actual exchange rate is:"} {props.rate} {", dated on:"}{" "}
+//       {props.date}
 //     </div>
 //   );
 // };
 
 // class CurrencyCheck extends React.Component {
 //   state = {
-//     amount: ""
-//   };
-
-//   currencyDataTableAll = [];
-
-//   getCurrencyData = () => {
-//     fetch(urlCurrency + "GBP")
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         let currencyDataGBP = {
-//           id: 1,
-//           code: resp.code,
-//           name: resp.currency,
-//           rate: resp.rates[0].mid
-//         };
-//         this.currencyDataTableAll.push(currencyDataGBP);
-//       });
-//     fetch(urlCurrency + "USD")
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         let currencyDataUSD = {
-//           id: 2,
-//           code: resp.code,
-//           name: resp.currency,
-//           rate: resp.rates[0].mid
-//         };
-//         this.currencyDataTableAll.push(currencyDataUSD);
-//       });
-//     fetch(urlCurrency + "EUR")
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         let currencyDataEUR = {
-//           id: 3,
-//           code: resp.code,
-//           name: resp.currency,
-//           rate: resp.rates[0].mid
-//         };
-//         this.currencyDataTableAll.push(currencyDataEUR);
-//       });
-//     fetch(urlCurrency + "CHF")
-//       .then(resp => resp.json())
-//       .then(resp => {
-//         let currencyDataCHF = {
-//           id: 4,
-//           code: resp.code,
-//           name: resp.currency,
-//           rate: resp.rates[0].mid,
-//           date: resp.rates[0].effectiveDate
-//         };
-//         this.currencyDataTableAll.push(currencyDataCHF);
-//       });
+//     amount: "",
+//     currencyChosen: "GBP"
 //   };
 
 //   handleChange = event => {
@@ -202,26 +78,42 @@
 //     });
 //   };
 
+//   handleSelect = event => {
+//     this.setState({
+//       currencyChosen: event.target.value
+//     });
+//   };
+
 //   render() {
-//     const { amount } = this.state;
-//     const calculators = this.currencyDataTableAll.map(currency => (
+//     const { amount, currencyChosen } = this.state;
+//     const calculators = currencyDataTableAll.map(currency => (
 //       <Cash
 //         key={currency.id}
 //         code={currency.code}
 //         rate={currency.rate}
 //         name={currency.name}
 //         cash={amount}
+//         date={currency.date}
 //       />
 //     ));
 //     return (
 //       <div className="app">
-//         {/* {(window.onload = this.getCurrencyData())} */}
-//         <button onClick={this.getCurrencyData}>getCurrencyData</button>
+//         <label>
+//           Choose currency
+//           <select value={currencyChosen} onChange={this.handleSelect}>
+//             <option value="GBP">GBP</option>
+//             <option value="USD">USD</option>
+//             <option value="EUR">EUR</option>
+//             <option value="CHF">CHF</option>
+//           </select>
+//         </label>
+//         <br />
 //         <label>
 //           <input
 //             type="number"
 //             value={this.state.amount}
 //             onChange={this.handleChange}
+//             placeholder="type a number in PLN"
 //           />
 //         </label>
 //         {calculators}
@@ -232,107 +124,93 @@
 
 // ReactDOM.render(<CurrencyCheck />, document.getElementById("root"));
 
-//////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-
-const urlCurrency = "https://api.nbp.pl/api/exchangerates/rates/a/";
-
-let currencyDataTableAll = [];
-
-const getCurrencyData = () => {
-  fetch(urlCurrency + "GBP")
-    .then(resp => resp.json())
-    .then(resp => {
-      let currencyDataGBP = {
-        id: 1,
-        code: resp.code,
-        name: resp.currency,
-        rate: resp.rates[0].mid
-      };
-      currencyDataTableAll.push(currencyDataGBP);
-    });
-  fetch(urlCurrency + "USD")
-    .then(resp => resp.json())
-    .then(resp => {
-      let currencyDataUSD = {
-        id: 2,
-        code: resp.code,
-        name: resp.currency,
-        rate: resp.rates[0].mid
-      };
-      currencyDataTableAll.push(currencyDataUSD);
-    });
-  fetch(urlCurrency + "EUR")
-    .then(resp => resp.json())
-    .then(resp => {
-      let currencyDataEUR = {
-        id: 3,
-        code: resp.code,
-        name: resp.currency,
-        rate: resp.rates[0].mid
-      };
-      currencyDataTableAll.push(currencyDataEUR);
-    });
-  fetch(urlCurrency + "CHF")
-    .then(resp => resp.json())
-    .then(resp => {
-      let currencyDataCHF = {
-        id: 4,
-        code: resp.code,
-        name: resp.currency,
-        rate: resp.rates[0].mid,
-        date: resp.rates[0].effectiveDate
-      };
-      currencyDataTableAll.push(currencyDataCHF);
-    });
-};
-
-window.onload = getCurrencyData();
+/////////////////////////////////////////////////////////////////////////////////////
 
 const Cash = props => {
   const value = (props.cash / props.rate).toFixed(2);
   return (
     <div>
-      {props.cash} {"PLN You can exchange for:"} {props.cash <= 0 ? "" : value}{" "}
-      {props.code}
+      {props.cash} {"PLN You can exchange for:"} {props.cash < 0 ? "" : value}{" "}
+      {props.code} {", actual exchange rate is:"} {props.rate} {", dated on:"}{" "}
+      {props.date}
     </div>
   );
 };
 
 class CurrencyCheck extends React.Component {
   state = {
-    amount: ""
+    currencies: [],
+    rates: [],
+    amount: 1,
+    currency: "USD",
+    secondCurrency: "EUR"
   };
 
-  handleChange = event => {
+  getCurrencyData = base => {
+    const urlApi = `https://api.exchangeratesapi.io/latest?base=${base}`;
+
+    fetch(urlApi)
+      .then(resp => {
+        return resp.json();
+      })
+      .then(data =>
+        this.setState({
+          rates: data["rates"],
+          // currencies: Object.keys(data['rates']).sort()
+          currencies: Object.keys(data["rates"]) //moje
+        })
+      );
+  };
+
+  changeAmount = event => {
     this.setState({
       amount: event.target.value
     });
   };
 
+  changeCurrency = event => {
+    this.setState({
+      currency: event.target.value
+    });
+    // this.getCurrencyData(event.target.value)
+    this.getCurrencyData(currency); //moje
+  };
+
+  changeSecondCurrency = event => {
+    this.setState({
+      secondCurrency: event.target.value
+    });
+    // this.getCurrencyData(secondCurrency) //skasowane?
+  };
+
+  currencyConvert = (amount, changeCrrency, rates) => {
+    return Number.parseFloat(amount * rates[changeCurrency]).toFixed(2);
+  };
+
+  // handleSelect = event => {
+  //   this.setState({
+  //     currencyChosen: event.target.value
+  //   });
+  // };
+
   render() {
-    const { amount } = this.state;
-    const calculators = currencyDataTableAll.map(currency => (
-      <Cash
-        key={currency.id}
-        code={currency.code}
-        rate={currency.rate}
-        name={currency.name}
-        cash={amount}
-      />
+    const { amount, rates, currency, currencies, currencyConvert } = this.state;
+    const currencyOption = currencies.map(currency => (
+      <option key={currency} value={currency}>
+        {currency}
+      </option>
     ));
     return (
       <div className="app">
-        {/* {(window.onload = this.getCurrencyData())} */}
-        <button onClick={this.getCurrencyData}>getCurrencyData</button>
-        <label>
-          <input
-            type="number"
-            value={this.state.amount}
-            onChange={this.handleChange}
-          />
-        </label>
-        {calculators}
+        <div className="container">
+          <label>
+            base currency
+            {currency}
+            <select name="currency" value={currency} onChange={changeCrrency}>
+              <option>{currency}</option>
+            </select>
+          </label>
+        </div>
       </div>
     );
   }
